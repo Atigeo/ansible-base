13718af5da95c74114559734cde8bc6e34379b4e
Greg <gregroy.abdo@atigeo.com>
2016-01-20 11:46:15 -0800

    Just making some comments

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,7 +1,16 @@
 #i!/bin/bash
 
+# Download the pip install script.
 wget https://bootstrap.pypa.io/get-pip.py
+
+# Pip is the easiest way to install ansible.
 sudo python get-pip.py
+
+# Ansible install
 sudo pip install ansible
+
+# docker-py is required for some roles
 sudo pip install docker-py
+
+# Install all the required roles.
 sudo ansible-galaxy install -r requirements.yml
